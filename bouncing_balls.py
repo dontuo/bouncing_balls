b2588d40b36e69ffe5da041e039b4f467184c1ad
dontuo <42874114+dontuo@users.noreply.github.com>
2022-10-13 17:57:55 +0300

    Update bouncing_balls.py

--- bouncing_balls.py
+++ bouncing_balls.py
@@ -10,27 +10,29 @@ class BouncingBalls:
         self.vectorbally = []
         self.vectx = 0
         self.vecty = 0
-        self.amount = 5
+        self.amount = 4
         self.vector = 0
         self.width = 64
         self.height = 48
+        self.a = 0
         
         i2c = I2C(sda=Pin(4), scl=Pin(5))
         self.display = ssd1306.SSD1306_I2C(64, 48, i2c)
         
         self.x1 = 0
         self.y1 = 0
+        self.meter = 5
     def take_random(self):
         for i in range(0, self.amount):
-            self.coordinateballs.append([self._randint(10, 50),self._randint(3, 25)])
+            self.coordinateballs.append([self._randint(10, 50),self._randint(5, 25)])
 
         for i in range(0, self.amount):
-            vectx = self._randint(2, 3)
-            vecty = self._randint(2, 3)
+            vectx = self._randint(1, 3)
+            vecty = self._randint(1, 3)
             stan = self._randint(0,1)
             if stan:
-                vectx = self._randint(1, 3) * -1
-                vecty = self._randint(1, 3) * -1
+                vectx = self._randint(1, 2) * -1
+                vecty = self._randint(1, 2) * -1
                 stan = 0
             self.vectorballx.append(vectx)
             self.vectorbally.append(vecty)
@@ -39,6 +41,7 @@ class BouncingBalls:
         self.take_random()
         while True:
             self.display.fill(0)
+            self._draw_lines()
             self._draw_balls()
             #utime.sleep(0.3)
             self.display.show()
@@ -47,34 +50,50 @@ class BouncingBalls:
             self.x = i[0]
             self.y = i[1]
             
-            if self.x >= self.width - 3: self.vectorballx[self.vector] = -self.vectorballx[self.vector]
-            elif self.x <= 0: self.vectorballx[self.vector] = self.vectorballx[self.vector] * -1
-            if self.y >= self.height - 3: self.vectorbally[self.vector] = -self.vectorbally[self.vector]
-            elif self.y <= 3: self.vectorbally[self.vector] = self.vectorbally[self.vector] * -1
+            
+            if self.x + 3>= self.width - 1: self.vectorballx[self.vector] = -self.vectorballx[self.vector]
+            elif self.x <= 2: self.vectorballx[self.vector] *= -1
+            if self.y >= self.height - 4: self.vectorbally[self.vector] = -self.vectorbally[self.vector]
+            elif self.y + 3 <= 4: self.vectorbally[self.vector] *= -1
             
             self.x += self.vectorballx[self.vector]
             self.y += self.vectorbally[self.vector]
-            #for b in self.coordinateballs:
-            #    x = b[0]
-            #    y = b[1]
-            #    self.display.line(self.x + 1, self.y + 1, x, y,1) 
+            self.coordinateballs2.append([self.x, self.y])
+            for b in self.coordinateballs2:
+                x = b[0]
+                y = b[1]
+                
+                #self.display.line(self.x + 1, self.y + 1, x + 1, y + 1,1) 
             
             self.display.fill_rect(self.x, self.y, 3, 3, 1)
             #self.display.show()
             #utime.sleep(0.3)
-            self.coordinateballs2.append([self.x, self.y])
+            self.a += 1
             self.vector += 1
             
-
+        self.a = 0
         self.vector = 0
         self.coordinateballs = self.coordinateballs2
         self.coordinateballs2 = []
         
+    def _find_dist_from_xy(self, zx, zy, x1, y1):
+        x2 = zx - x1
+        y2 = zy - y1
+        self.display.fill(0)
+        if x2 < 0: x2 *= -1
+        elif y2 < 0: y2 *= -1
+        if x2 <= self.meter and y2 <= self.meter: return True
+    
     def _randint(self, min, max):
         span = max - min + 1
         div = 0x3fffffff // span
         offset = urandom.getrandbits(30) // div
         val = min + offset
         return val
-    
+    def _draw_lines(self):
+        self.display.line(0, 0, 64, 0, 1)
+        self.display.line(0, 0, 0, 48, 1)
+        self.display.line(63, 0, 63, 48, 1)
+        self.display.line(63, 47, 1, 47, 1)
+
 BouncingBalls().main_loop()
